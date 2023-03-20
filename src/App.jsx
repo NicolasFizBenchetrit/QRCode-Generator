3fba5a372ad0150b874b09aa6b743da4f19f8735
NicolasFizBenchetrit <nicolasfiz@outlook.com>
2023-03-20 16:19:04 +0100

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Input from './components/Input'
 import QRCode from 'qrcode'
 import { useState } from 'react'
 import Qr from './components/Qr'
+import { toast, Toaster } from 'react-hot-toast'
 
 function App () {
   const [qrcode, setQrcode] = useState('')
@@ -17,14 +18,17 @@ function App () {
       margin: 1
     }
     QRCode.toDataURL(urlText, opts, (err, url) => {
-      if (err) return console.error(err)
+      if (err) {
+        toast.error('El texto esta varcío')
+      }
       setQrcode(url)
       setGenerated(true)
     })
   }
 
   return (
-    <div className='flex flex-col p-3 m-5'>
+    <div className='flex flex-col p-3 m-8'>
+      <Toaster />
       <h1 className='mb-5 text-center text-2xl font-semibold'>
         QRCode Generator
       </h1>
